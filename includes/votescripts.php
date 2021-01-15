7e9d745d413966bc4660e3ea4a343a3e30c12aa1
AxelDeRoeck <axelderoeck22@gmail.com>
2021-01-15 16:02:56 +0100

    test fix button

--- includes/votescripts.php
+++ includes/votescripts.php
@@ -1,47 +1,8 @@
-<?php 
-
-ob_start();
-require_once("includes/dbconn.inc.php");
-session_start();
-
-//mysqli_close($dbconn); //CLOSE DOEN
-
+<?php
+    header('Content-Type: text/javascript; charset=UTF-8');
 ?>
 
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-  <?php include "includes/headinfo.php"; ?>
-</head>
-<body>
-  <?php include "includes/navigation.php"; ?>
-  
-  <div class="infoDiv">
-        <h1>WIE IS DE <span>MOL</span> ?</h1>
-        <p><span>Swipe</span> tussen de kandidaten en <span>stem</span>.</p>
-  </div>
-  
-  <form id="deMolForm"></form>
-
-  <div class="swiper-container">
-    <div id="carousel" class="swiper-wrapper">
-   
-      <!-- dynamische items -->
-         
-    </div>
-  </div>
-    
-  <div class="submitDiv">
-    <input form="deMolForm" class="formSubmitBtn" type="submit" value="Inzenden">
-  </div>
-
-  <!-- JavaScript --> 
-  <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
-  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-<!-- <script type="text/javascript" src="js/scripts.js"></script> -->
-
-  <script>
-    window.addEventListener('load', function() {
+window.addEventListener('load', function() {
       //PHP waardes in array steken
       let deelnemers = [
       <?php
@@ -139,9 +100,3 @@ session_start();
       }     
       document.getElementById(id).value = value;
     }
-  </script>
-
-
-<?php mysqli_close($dbconn); ?>
-</body>
-</html>
