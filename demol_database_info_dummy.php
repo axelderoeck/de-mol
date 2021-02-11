ed489d6ea7bdf65ca1951f3785189119a86adc37
AxelDeRoeck <axelderoeck22@gmail.com>
2021-02-11 22:43:59 +0100

    extra database security

---
+++ demol_database_info_dummy.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "localhost or link";
+$database = "databaseName";
+$username = "username";
+$password = "password";
+
+?>
--- includes/dbconn.inc.php
+++ includes/dbconn.inc.php
@@ -1,13 +1,9 @@
 <?php
-define("SERVERNAME", "sql189.main-hosting.eu"); //lokaal: sql189.main-hosting.eu, online: localhost
-define("USERNAME", "u939917173_Aksol");
-define("PASSWORD", "Axelsnow1973");
-define("DATABASE", "u939917173_demol");
-//connectie maken met de databank
-$dbconn = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DATABASE);
 
- //testen of de connectie is gemaakt
- //var_dump($dbconn);
+//include "./demoldatabaseinfo.php"; //for development purposes
 
+include "./../demol_database_info.php"; //external file above project folder level
+
+$dbconn = mysqli_connect($servername,$username,$password,$database);
 
 ?>
