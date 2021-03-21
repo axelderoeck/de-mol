60291b83ea1971ea0eed474de9770d7f184f881b
AxelDeRoeck <axelderoeck22@gmail.com>
2021-03-21 22:39:32 +0100

    test

--- includes/settings.php
+++ includes/settings.php
@@ -3,7 +3,7 @@
   $seizoen_eind = "05/23/2021"; // maand/dag/jaar    echte: 05/23/2021
   $aantal_kandidaten = 10; // hoeveel kandidaten in het spel
   $top_aantal = 20; // algemene ranglijst top ... tonen
-  $admins = array(7, 10); // array van alle admin's hun ID
+  $admins = array(7); // array van alle admin's hun ID
   $stemmen_dag = 'Sun'; // dag waarop gestemd wordt
   $stemmen_uur = "2200"; // vanaf dit uur mag er gestemd worden
 
