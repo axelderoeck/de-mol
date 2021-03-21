9265406a68dbba10533d98f81b73f4c622f02112
AxelDeRoeck <axelderoeck22@gmail.com>
2021-03-21 16:24:13 +0100

    aaa

--- includes/settings.php
+++ includes/settings.php
@@ -5,7 +5,7 @@
   $top_aantal = 20; // algemene ranglijst top ... tonen
   $admins = array(7, 10); // array van alle admin's hun ID
   $stemmen_dag = 'Sun'; // dag waarop gestemd wordt
-  $stemmen_uur = "1600"; // vanaf dit uur mag er gestemd worden
+  $stemmen_uur = "2200"; // vanaf dit uur mag er gestemd worden
 
   // bericht tonen op de home page
   // bericht settings
