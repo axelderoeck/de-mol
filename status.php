390e5c56db3bdceeb792ade3be39b2e53d2bcb0f
AxelDeRoeck <axelderoeck22@gmail.com>
2021-03-30 22:09:27 +0200

    if kandidate is out -> bottom status

--- status.php
+++ status.php
@@ -17,6 +17,14 @@ ON table_Scores.Identifier = table_Kandidaten.Identifier
 GROUP BY table_Scores.Identifier
 ORDER BY SUM(Score) DESC";
 
+$getMostVotedFollowers = "SELECT Naam, SUM(Score), Visibility
+FROM table_Scores
+LEFT JOIN table_Kandidaten
+ON table_Scores.Identifier = table_Kandidaten.Identifier
+WHERE UserId IN (SELECT UserIsFollowingId FROM table_Followers WHERE UserId = '$id')
+GROUP BY table_Scores.Identifier
+ORDER BY SUM(Score) DESC";
+
 $getAmountVoted = "SELECT COUNT(Id) FROM table_Users WHERE Voted = 1";
 if ($stmtGetAmountVoted = mysqli_prepare($dbconn, $getAmountVoted)){
     mysqli_stmt_execute($stmtGetAmountVoted);
@@ -33,11 +41,24 @@ if ($stmtGetTotalVoted = mysqli_prepare($dbconn, $getTotalVoted)){
 }
 mysqli_stmt_fetch($stmtGetTotalVoted);
 
+$getTotalVotedFollowers = "SELECT DISTINCT SUM(Score) FROM table_Scores
+WHERE UserId IN (SELECT UserIsFollowingId FROM table_Followers WHERE UserId = '$id')
+";
+if ($stmtGetTotalVotedFollowers = mysqli_prepare($dbconn, $getTotalVotedFollowers)){
+    mysqli_stmt_execute($stmtGetTotalVotedFollowers);
+    mysqli_stmt_bind_result($stmtGetTotalVotedFollowers, $totalVotedFollowers);
+    mysqli_stmt_store_result($stmtGetTotalVotedFollowers);
+}
+mysqli_stmt_fetch($stmtGetTotalVotedFollowers);
+
 ?>
 
 <!DOCTYPE html>
 <html lang="nl">
 <head>
+  <!--
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css'>
+  -->
   <?php include "includes/headinfo.php"; ?>
 </head>
 <body>
@@ -50,39 +71,74 @@ mysqli_stmt_fetch($stmtGetTotalVoted);
 
     <h1>Statistieken</h1>
     <h2>Verdenkingen</h2>
+    <!--
+    <ul id="tabs-swipe-demo" class="tabs">
+      <li class="tab col s3"><a class="active" href="#swipe-2">Volgend</a></li>
+      <li class="tab col s3"><a href="#swipe-1">Iedereen</a></li>
+    </ul> -->
+    <div class="slide-container">
+      <div class="slide-wrapper">
+        <div id="swipe-1">
       <?php
           if($result = mysqli_query($dbconn, $getMostVoted)){
               if(mysqli_num_rows($result) > 0){
                   while($row = mysqli_fetch_array($result)){
-                    $percentCalc = round(($row['SUM(Score)'] / $totalVoted) * 100, 2);
-                    $percentScore = explode(".", $percentCalc);
-                    if ($row['Visibility'] == 'out') {
-                      $outClass = 'class="isOut"';
-                      $outClass2 = "isOut2";
-                    }else{
-                      $outClass = "";
-                      $outClass2 = "";
+                    if ($row['Visibility'] != 'out') {
+                      $percentCalc = round(($row['SUM(Score)'] / $totalVoted) * 100, 2);
+                      $percentScore = explode(".", $percentCalc);
+                      ?>
+                      <div class="status">
+                        <p><?php echo $row['Naam']; ?> - <span class="percent"><?php echo $percentScore[0]; ?><span class="smaller">.<?php echo $percentScore[1]; ?></span>%</span></p>
+                      </div>
+                      <div class="meter">
+                        <span style="width: <?php echo $percentScore[0]; ?>%"></span>
+                      </div>
+                      <?php
                     }
-                    ?>
-                    <div class="status">
-                      <p><?php echo $row['Naam']; ?> - <span class="percent <?php echo $outClass2; ?>"><?php echo $percentScore[0]; ?><span class="smaller <?php echo $outClass2; ?>">.<?php echo $percentScore[1]; ?></span>%</span></p>
-                    </div>
-                    <div class="meter">
-                      <span <?php echo $outClass; ?> style="width: <?php echo $percentScore[0]; ?>%"></span>
-                    </div>
-                    <?php
+                  }
+                  // Free result set
+                  mysqli_free_result($result);
+              }
+          }
+          if($result = mysqli_query($dbconn, $getMostVoted)){
+              if(mysqli_num_rows($result) > 0){
+                  while($row = mysqli_fetch_array($result)){
+                    if ($row['Visibility'] == 'out') {
+                      $percentCalc = round(($row['SUM(Score)'] / $totalVoted) * 100, 2);
+                      $percentScore = explode(".", $percentCalc);
+                      ?>
+                      <div class="status">
+                        <p><?php echo $row['Naam']; ?> - <span class="percent isOut2"><?php echo $percentScore[0]; ?><span class="smaller isOut2">.<?php echo $percentScore[1]; ?></span>%</span></p>
+                      </div>
+                      <div class="meter">
+                        <span class="isOut" style="width: <?php echo $percentScore[0]; ?>%"></span>
+                      </div>
+                      <?php
+                    }
                   }
                   // Free result set
                   mysqli_free_result($result);
               }
           }
       ?>
+        </div>
+        <!--
+        <div id="swipe-2">
+
+        </div>
+      -->
+      </div>
+    </div>
       <p class="example"><?php echo $amountVoted; ?> <?php if ($amountVoted == 1) {echo "mollenjager heeft";}else{echo "mollenjagers hebben";} ?>  gestemd deze week.</p>
 
     </div>
   </div>
 
   <script type="text/javascript" src="js/scripts.js"></script>
+
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js'></script>
+
   <?php mysqli_close($dbconn); ?>
 </body>
 </html>
