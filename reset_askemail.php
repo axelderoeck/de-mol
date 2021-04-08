f4ceb9474409b2b3e5ff6292fce188ddaae9c9e1
AxelDeRoeck <axelderoeck22@gmail.com>
2021-04-08 21:23:48 +0200

    fix???

--- reset_askemail.php
+++ reset_askemail.php
@@ -27,8 +27,8 @@ if (isset($_POST["sendMail"])){
 
     // set a random unique key to the user
     $queryGiveUserKey = "UPDATE table_Users
-    SET UserKey = 'test'
-    WHERE Id = $id";
+    SET UserKey = '$randomGeneratedString'
+    WHERE Id = '$id'";
     mysqli_query($dbconn, $queryGiveUserKey);
 
     // set mail values
