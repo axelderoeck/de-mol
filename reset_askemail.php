2fc9118b1d21b4890de3ab26d8efe8d9bc17ecc7
AxelDeRoeck <axelderoeck22@gmail.com>
2021-04-08 21:49:19 +0200

    dumbass fix

--- reset_askemail.php
+++ reset_askemail.php
@@ -28,7 +28,7 @@ if (isset($_POST["sendMail"])){
     // set a random unique key to the user
     $dbconn->query("UPDATE table_Users
     SET UserKey = '$randomGeneratedString'
-    WHERE Id = '$id'");
+    WHERE Id = $id");
 
     // set mail values
     $subject = "Wachtwoord reset";
