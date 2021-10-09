80034d2f683239f4412d71db10463e79d224b85f
AxelDeRoeck <axelderoeck22@gmail.com>
2021-10-09 12:49:26 +0200

    added PDO to awards overview

--- awardslist.php
+++ awardslist.php
@@ -1,38 +1,25 @@
 <?php
 
-ob_start();
-require_once("includes/dbconn.inc.php");
-session_start();
+require_once("includes/phpdefault.php");
 
-$id = $_SESSION["Id"];
-
-if ($_SESSION["Id"] == NULL) {
-  header('location:index.php');
-}
-
-/* Get all awards that the user has and put it in array */
-
-$selectAwards = "SELECT AwardId
+// Get all achieved awards
+$stmt = $pdo->prepare('SELECT AwardId
 FROM table_UserAwards
 LEFT JOIN table_Awards
 ON table_UserAwards.AwardId = table_Awards.Id
-WHERE table_UserAwards.UserId = '$id'
-";
-
-$arrayUserAwards = array();
-
-if($executeSelectAwards = mysqli_query($dbconn, $selectAwards)){
-  while($row = mysqli_fetch_array($executeSelectAwards)){
-    array_push($arrayUserAwards, $row['AwardId']);
-  }
+WHERE table_UserAwards.UserId = ?');
+$stmt->execute([ $_SESSION["Id"] ]);
+$user_awards = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Add the award id's to array
+$user_awards_id = array();
+foreach($user_awards as $award){
+  array_push($user_awards_id, $award['AwardId']);
 }
 
-/* Get all awards available */
-
-$selectAllAwards = "SELECT Id, Naam, Beschrijving, Editie, Secret
-FROM table_Awards
-WHERE Actief = 1;
-";
+// Get all available awards
+$stmt = $pdo->prepare('SELECT * FROM table_Awards WHERE Actief = 1 ORDER BY Naam');
+$stmt->execute();
+$awards = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
@@ -46,51 +33,44 @@ WHERE Actief = 1;
 
   <div id="main">
     <div class="respContainer">
-    <a href="profiel.php?user=<?php echo $_SESSION["Id"] ?>"><img class="goBackArrow" src="img/assets/arrow.png" alt="arrow"></a>
+    <a href="profiel.php?user=<?=$_SESSION["Id"] ?>"><img class="goBackArrow" src="img/assets/arrow.png" alt="arrow"></a>
     <div class="awardslist">
-      <?php
-      $delay = 0; //set animation delay value
+      <?php $delay = 0; /* Animation delay value */ ?>
 
-      // show all unlocked awards
-      if($result = mysqli_query($dbconn, $selectAllAwards)){
-        while($row = mysqli_fetch_array($result)){
-          if (in_array($row['Id'], $arrayUserAwards)) { ?>
-            <div class="info" style="animation-delay: <?php echo $delay/4; ?>s;" >
-              <img src="img/awards/<?php echo $row['Id']; ?>.png" alt="award foto van <?php echo $row['Naam']; ?>">
-              <h3><?php echo $row['Naam']; ?></h3>
-              <p><?php echo $row['Beschrijving']; ?></p>
-              <i class="fas fa-unlock"></i>
-            </div>
-            <?php $delay++; //increase animation delay value
-          }
-        }
-      }
-
-      // show all locked awards
-      if($result = mysqli_query($dbconn, $selectAllAwards)){
-        while($row = mysqli_fetch_array($result)){
-          if (!in_array($row['Id'], $arrayUserAwards)) { ?>
-            <div class="locked" style="animation-delay: <?php echo $delay/4; ?>s;" >
-              <img src="img/awards/<?php echo $row['Id']; ?>.png" alt="award foto van <?php echo $row['Naam']; ?>">
-              <h3 style="color: #707070;"><?php echo $row['Naam']; ?></h3>
-              <p style="color: #707070;"><?php echo $row['Beschrijving']; ?></p>
-              <?php
+      <!-- Unlocked Awards -->
+      <?php foreach($awards as $award): ?>
+        <?php if (in_array($award['Id'], $user_awards_id)): ?>
+          <div class="info" style="animation-delay: <?=$delay/4; ?>s;" >
+            <img src="img/awards/<?=$award['Id']; ?>.png" alt="award foto van <?=$award['Naam']; ?>">
+            <h3><?=$award['Naam']; ?></h3>
+            <p><?=$award['Beschrijving']; ?></p>
+            <i class="fas fa-unlock"></i>
+          </div>
+          <?php $delay++; ?>
+        <?php endif; ?>
+      <?php endforeach; ?>
+        
+      <!-- Locked Awards -->
+      <?php foreach($awards as $award): ?>
+        <?php if (!in_array($award['Id'], $user_awards_id)): ?>
+          <div class="info" style="animation-delay: <?=$delay/4; ?>s;" >
+            <img src="img/awards/<?=$award['Id']; ?>.png" alt="award foto van <?=$award['Naam']; ?>">
+            <h3 style="color: #707070;"><?=$award['Naam']; ?></h3>
+            <p style="color: #707070;"><?=$award['Beschrijving']; ?></p>
+            <?php
               // if this record is the secret award -> set hidden code
-                if ($row['Secret'] == 1) {echo "<p class='hiddenField'>" . $award_secret_mol_randomcode . "</p>";}
-              ?>
-              <i style="color: #707070;" class="fas fa-lock"></i>
-            </div>
-            <?php $delay++; //increase animation delay value
-          }
-        }
-      }
-      ?>
+              if ($award['Secret'] == 1) {echo "<p class='hiddenField'>" . $award_secret_mol_randomcode . "</p>";}
+            ?>
+            <i style="color: #707070;" class="fas fa-lock"></i>
+          </div>
+          <?php $delay++; ?>
+        <?php endif; ?>
+      <?php endforeach; ?>
 
     </div>
     </div>
   </div>
 
   <script type="text/javascript" src="js/scripts.js"></script>
-  <?php mysqli_close($dbconn); ?>
 </body>
 </html>
