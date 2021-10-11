2db37bea376b2ffea31bb2a8a70c4afed48a3e4c
AxelDeRoeck <axelderoeck22@gmail.com>
2021-10-11 20:39:51 +0200

    updated delete method with PDO

--- includes/account-actions/deleteaccount.php
+++ includes/account-actions/deleteaccount.php
@@ -1,43 +1,30 @@
 <?php
 
-ob_start();
-require_once("includes/dbconn.inc.php");
-session_start();
-
-if ($_SESSION["Id"] == NULL) {
-  header('location:index.php');
-}
-
 if (isset($_POST["deleteAccount"])){
-  $id = $_SESSION["Id"];
+  // Delete the user
+  $stmt = $pdo->prepare('DELETE FROM table_Users WHERE Id = ?');
+  $stmt->execute([ $_SESSION["Id"] ]);
 
-  $dbconn->query("DELETE FROM table_Users
-    WHERE Id = '$id';
-    ");
+  // Delete the scores from user
+  $stmt = $pdo->prepare('DELETE FROM table_Scores WHERE UserId = ?');
+  $stmt->execute([ $_SESSION["Id"] ]);
 
-  $dbconn->query("DELETE FROM table_Scores
-    WHERE UserId = '$id';
-    ");
+  // Delete the awards from user
+  $stmt = $pdo->prepare('DELETE FROM table_UserAwards WHERE UserId = ?');
+  $stmt->execute([ $_SESSION["Id"] ]);
 
-  $dbconn->query("DELETE FROM table_UserAwards
-    WHERE UserId = '$id';
-    ");
+  // Delete the user from friends list
+  $stmt = $pdo->prepare('DELETE FROM table_Friends WHERE Id = ? OR IsFriendsWithId = ?');
+  $stmt->execute([ $_SESSION["Id"], $_SESSION["Id"] ]);
 
-    $dbconn->query("DELETE FROM table_Followers
-      WHERE UserId = '$id';
-    ");
-
-    $dbconn->query("DELETE FROM table_Followers
-      WHERE UserIsFollowingId = '$id';
-    ");
-
-    $_SESSION["Id"] = NULL;
-    $_SESSION["Naam"] = "";
-    $_SESSION["Gebruikersnaam"] = "";
-    $_SESSION["Voted"] = 0;
-    $_SESSION["Admin"] = 0;
-    session_destroy();
-    header('location:index.php');
+  // Reset session
+  $_SESSION["Id"] = NULL;
+  $_SESSION["Naam"] = "";
+  $_SESSION["Gebruikersnaam"] = "";
+  $_SESSION["Voted"] = 0;
+  $_SESSION["Admin"] = 0;
+  session_destroy();
+  header('location:index.php');
 }
 
 
