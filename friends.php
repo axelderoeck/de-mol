e2dfb7a8e3facb0a9d656f707a9091111a0de958
AxelDeRoeck <axelderoeck22@gmail.com>
2021-12-11 13:45:04 +0100

    updated friends style

--- friends.php
+++ friends.php
@@ -3,7 +3,7 @@
 require_once("includes/phpdefault.php");
 
 // Get all friends from user
-$stmt = $pdo->prepare('SELECT table_Users.Id, Username, Friendcode, Voted, Highscore
+$stmt = $pdo->prepare('SELECT *
                       FROM table_Users
                       LEFT JOIN table_Friends
                       ON table_Users.Id = table_Friends.IsFriendsWithId
@@ -16,32 +16,20 @@ $friends = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <?php include "includes/header.php"; ?>
 
     <a href="home.php"><img class="goBackArrow" src="img/assets/arrow.png" alt="arrow"></a>
-    <h1>Mollenjagers</h1>
-    <p class="example">Hier kan je al jouw mede-mollenjagers vinden. <br>
-    <i class='fas fa-check-circle'></i> duid aan wie er al gestemd heeft.</p>
+    <h1>Vrienden</h1>
+    <p class="example">Hier kan je al jouw mede-mollenjagers vinden.</p>
 
-    <div class="friends">
       <?php $i = 0; foreach($friends as $friend): ?>
-      <div class="friend">
-        <img src="img/assets/demol_logo_geen_tekst.png" alt="de mol logo">
-        <span><?=$friend["Username"]?></span>
-      </div>
-      <?php $i++; endforeach; ?>
-    </div>
-
-    <div class="deelnemersList">
-      <?php if(!empty($friends)): ?>
-      <?php $i = 0; foreach($friends as $friend): ?>
-      <a class="deelnemerItem info" style="animation-delay: <?=$i/6?>s;" href="profile.php?u=<?=$friend['Id']?>">
-        <i class='fas fa-user left'></i>
-          <?=$friend["Username"]?>
-          <?php if($friend["Voted"] == 1) {echo "<i class='fas fa-check-circle right'></i>";}?>
+      <a href="profile.php?u=<?=$friend["IsFriendsWithId"]?>">
+        <div class="displayUser">
+          <div>
+            <span><?=$friend["Score"]?></span>
+            <img src="img/assets/demol_logo_geen_tekst_groen.png" alt="de mol logo">
+          </div>
+          <span><?=$friend["Username"]?></span>
+        </div>
       </a>
       <?php $i++; endforeach; ?>
-      <?php else: ?>
-      <h2>Je hebt nog geen spelers toegevoegd.<br>Voeg er toe door op de knop hieronder te klikken.</h2>
-      <?php endif; ?>
-    </div>
 
     <hr>
     <button onclick="location.href = 'adduser.php';" class="styledBtn" type="submit" name="button">Voeg spelers toe</button>
--- includes/footer.php
+++ includes/footer.php
@@ -6,27 +6,5 @@
 <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 
-<!-- Delete later -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-<script type="text/javascript">
-    $(document).ready(function(){
-      // Slick settings for candidate list
-      $('.friends').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: false,
-        centerMode: true,
-        centerPadding: '5%',
-        focusOnSelect: true,
-        draggable: true,
-        mobileFirst: true,
-        infinite: true,
-        swipe: true
-      });
-    });
-</script>
-
 </body>
 </html>
