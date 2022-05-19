a0cc2bc670417a46a317c2c71c995e46fc87f837
Axel De Roeck <axelderoeck22@gmail.com>
2022-05-19 15:18:24 +0200

    clear db info
    it's offline anyway

--- demol/db/db_info.php
+++ demol/db/db_info.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "sql189.main-hosting.eu";
-$database = "u939917173_demol";
-$username = "u939917173_Aksol";
-$password = "M(km3fCmyBYh7j]8*1Yoq9k";
+$servername = "";
+$database = "";
+$username = "";
+$password = "";
 
 ?>
